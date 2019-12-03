a4a6d16a6fa6d43fb1a15caaf6899b41a8b5353b
Oleg_Romenskyi <oleg_romenskyi@epam.com>
2019-12-03 18:46:35 +0200

    fix locales

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_genesis_test/generated/i18n.dart';
 import 'package:flutter_genesis_test/ui/facebook_login_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,9 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        localizationsDelegates: [S.delegate],
+        localizationsDelegates: [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
         supportedLocales: S.delegate.supportedLocales,
-        localeResolutionCallback: S.delegate.resolution(fallback: Locale('en')),
         theme: ThemeData(
           primarySwatch: Colors.green,
         ),
